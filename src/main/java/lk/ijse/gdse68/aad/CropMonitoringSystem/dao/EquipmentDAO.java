bb84276af06e6da65fd4c7100e1557f6a099c10d
duranginethmini <durangineth02@gmail.com>
2024-11-09 12:12:43 +0530

    "DAO interfaces were added"

---
+++ src/main/java/lk/ijse/gdse68/aad/CropMonitoringSystem/dao/EquipmentDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.aad.CropMonitoringSystem.dao;
+
+import lk.ijse.gdse68.aad.CropMonitoringSystem.entity.EquipmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentDAO extends JpaRepository<EquipmentEntity,String> {
+}
---
+++ src/main/java/lk/ijse/gdse68/aad/CropMonitoringSystem/dao/VehicleDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.aad.CropMonitoringSystem.dao;
+
+import lk.ijse.gdse68.aad.CropMonitoringSystem.entity.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleDAO extends JpaRepository<VehicleEntity,String> {
+}
